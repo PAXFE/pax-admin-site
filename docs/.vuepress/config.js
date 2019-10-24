60c21a449f9a9791b76d51e516e993c4c0af5313
cqzongjian <cqzongjian@gmail.com>
2019-10-24 09:50:28 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 var nav = require('./nav.js')
-var { EcosystemNav, ComponentNav } = nav
+var { ComponentNav } = nav
 
 var utils = require('./utils.js')
 var { genNav, getComponentSidebar, deepClone } = utils
@@ -32,169 +32,47 @@ module.exports = {
     },
     locales: {
       '/': {
-        label: 'English',
-        selectText: 'Languages',
-        editLinkText: 'Edit this page on GitHub',
-        nav: [
-          {
-            text: 'Guide',
-            link: '/guide/'
-          },
-          {
-            text: 'Features',
-            items: genNav(deepClone(ComponentNav), 'EN')
-          },
-          {
-            text: 'Ecosystem',
-            items: genNav(deepClone(EcosystemNav), 'EN')
-          },
-          {
-            text: 'Donate',
-            link: '/donate/'
-          },
-          {
-            text: '中文站点(gitee)',
-            link: 'https://panjiachen.gitee.io/pax-admin-site/zh/'
-          }
-        ],
-        sidebar: {
-          '/guide/': [
-            {
-              title: 'Essentials',
-              collapsable: false,
-              children: genEssentialsSidebar()
-            },
-            {
-              title: 'Advanced',
-              collapsable: false,
-              children: genAdvancedSidebar()
-            },
-            {
-              title: 'Other',
-              collapsable: false,
-              children: [
-                '/guide/other/gitter.md',
-                '/guide/other/release-notes.md'
-              ]
-            }
-          ],
-          '/feature/component/': getComponentSidebar(
-            deepClone(ComponentNav),
-            'EN'
-          ),
-          '/feature/script/': [
-            '/feature/script/svgo.md',
-            '/feature/script/new.md'
-          ]
-        }
-      },
-      '/es/': {
-        label: 'Español',
-        selectText: 'Idiomas',
-        editLinkText: 'Editar esta página en GitHub',
-        nav: [
-          {
-            text: 'Guía',
-            link: '/es/guide/'
-          },
-          {
-            text: 'Características',
-            items: genNav(deepClone(ComponentNav), 'ES')
-          },
-          {
-            text: 'Ecosistema',
-            items: genNav(deepClone(EcosystemNav), 'ES')
-          },
-          {
-            text: 'Donar',
-            link: '/es/donate/'
-          }
-        ],
-        sidebar: {
-          '/es/guide/': [
-            {
-              title: 'Esenciales',
-              collapsable: false,
-              children: genEssentialsSidebar('/es')
-            },
-            {
-              title: 'Avanzado',
-              collapsable: false,
-              children: genAdvancedSidebar('/es')
-            },
-            {
-              title: 'Otro',
-              collapsable: false,
-              children: [
-                '/es/guide/other/gitter.md',
-                '/es/guide/other/release-notes.md'
-              ]
-            }
-          ],
-          '/es/feature/component/': getComponentSidebar(
-            deepClone(ComponentNav),
-            'ES'
-          ),
-          '/es/feature/script/': [
-            '/es/feature/script/svgo.md',
-            '/es/feature/script/new.md'
-          ]
-        }
-      },
-      '/zh/': {
         label: '简体中文',
         selectText: '选择语言',
         editLinkText: '在 GitHub 上编辑此页',
         nav: [
           {
             text: '指南',
-            link: '/zh/guide/'
+            link: '/guide/'
           },
           {
             text: '功能',
             items: genNav(deepClone(ComponentNav), 'ZH')
-          },
-          {
-            text: '生态系统',
-            items: genNav(deepClone(EcosystemNav), 'ZH')
-          },
-          {
-            text: '中文站点(gitee)',
-            link: 'https://panjiachen.gitee.io/pax-admin-site/zh/'
-          },
-          {
-            text: '招聘',
-            link: '/zh/job/'
           }
         ],
         sidebar: {
-          '/zh/guide/': [
+          '/guide/': [
             {
               title: '基础',
               collapsable: false,
-              children: genEssentialsSidebar('/zh')
+              children: genEssentialsSidebar('')
             },
             {
               title: '进阶',
               collapsable: false,
-              children: genAdvancedSidebar('/zh')
+              children: genAdvancedSidebar('')
             },
             {
               title: '其它',
               collapsable: false,
               children: [
-                '/zh/guide/other/faq.md',
-                '/zh/guide/other/release-notes.md'
+                '/guide/other/faq.md',
+                '/guide/other/release-notes.md'
               ]
             }
           ],
-          '/zh/feature/component/': getComponentSidebar(
+          '/feature/component/': getComponentSidebar(
             deepClone(ComponentNav),
             'ZH'
           ),
-          '/zh/feature/script/': [
-            '/zh/feature/script/svgo.md',
-            '/zh/feature/script/new.md'
+          '/feature/script/': [
+            '/feature/script/svgo.md',
+            '/feature/script/new.md'
           ]
         }
       }
@@ -202,16 +80,8 @@ module.exports = {
   },
   locales: {
     '/': {
-      lang: 'en-US',
-      description: 'A magical vue admin'
-    },
-    '/zh/': {
       lang: 'zh-CN',
       description: 'A magical vue admin'
-    },
-    '/es/': {
-      lang: 'es-ES',
-      description: 'Un administrador mágico de vue'
     }
   },
   configureWebpack: {
--- docs/guide/other/release-notes.md
+++ docs/guide/other/release-notes.md
@@ -1,11 +1,11 @@
-# Release Notes
+# 更新记录
 
-[Release Notes](https://github.com/PAXFE/vue-element-admin/releases)
+[在线查看更新记录](https://github.com/PAXFE/pax-admin/releases)
 
 <script>
 export default {
   mounted () {
-    window.open('https://github.com/PAXFE/vue-element-admin/releases')
+    window.open('https://github.com/PAXFE/pax-admin/releases')
   }
 }
 </script>
